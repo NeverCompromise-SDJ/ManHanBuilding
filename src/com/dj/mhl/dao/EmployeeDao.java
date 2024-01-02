40bc65b4b41d72572f8ebfa768ff02dfeca0032f
NeverCompromise-SDJ <1580987935@qq.com>
2024-01-03 00:11:26 +0800

    Employee Dao类创建

---
+++ src/com/dj/mhl/dao/EmployeeDao.java
@@ -0,0 +1,13 @@
+package com.dj.mhl.dao;
+
+import com.dj.mhl.domain.Employee;
+
+/**
+ * Employee对应的Dao类，可以包含特殊的操作，DAO层用于执行SQL语句
+ *
+ * @author SongDongJie
+ * @create 2024/1/2 - 23:37
+ */
+public class EmployeeDao extends BasicDao<Employee> {
+
+}
